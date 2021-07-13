95077f46cea0e0a2b3a3e0cfaf9a5127eb2f492c
diegoejp <d.jacobpulgar@gmail.com>
2021-07-13 14:46:04 -0400

    agregado registro no validado

--- src/views/register.js
+++ src/views/register.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import{FaEye} from "react-icons/fa"
+import { FaEye } from "react-icons/fa"
 
 
 const Register = () => {
@@ -7,62 +7,76 @@ const Register = () => {
     const [user, setUser] = useState("");
 
     const [isRevealPwd, setIsRevealPwd] = useState(false);
+    const [isRevealPwdC, setIsRevealPwdC] = useState(false);
 
     return (
 
         <>
             <div className="container">
-                <div>
+                <div className="text-center my-3">
                     <h1 className="display-1">
                         PetVA
                     </h1>
                 </div>
-                <div className="row">
+                <div className="row my-3">
                     <div className="col-12 col-md-5">
                         <h3 className="display-4">Bienvenido a PetVA</h3>
                         <p className="fs-4">Para continuar tu proceso de registro selecciona que tipo de usuario eres:</p>
                         <div >
 
                             <div className="form-check form-check-inline d-block fs-3">
-                                <input onChange={(e)=>setUser(e.target.value)} className="form-check-input" type="radio" name="tipoUsuario" id="optNormal" value="normal" />
+                                <input onChange={(e) => setUser(e.target.value)} className="form-check-input" type="radio" name="tipoUsuario" id="optNormal" value="NORMAL" />
                                 <label className="form-check-label" htmlFor="optNormal">Usuario Común</label>
                             </div>
                             <div className="form-check form-check-inline d-block fs-3">
-                                <input onChange={(e) => setUser(e.target.value)} className="form-check-input" type="radio" name="tipoUsuario" id="optClinica" value="clinica" />
+                                <input onChange={(e) => setUser(e.target.value)} className="form-check-input" type="radio" name="tipoUsuario" id="optClinica" value="CLINICA" />
                                 <label className="form-check-label" htmlFor="optClinica">Clínica Veterinaria</label>
                             </div>
                             <div className="form-check form-check-inline d-block fs-3">
-                                <input  onChange={(e) => setUser(e.target.value)} className="form-check-input" type="radio" name="tipoUsuario" id="optFundacion" value="fundacion" />
+                                <input onChange={(e) => setUser(e.target.value)} className="form-check-input" type="radio" name="tipoUsuario" id="optFundacion" value="FUNDACION" />
                                 <label className="form-check-label" htmlFor="optFundacion">Fundación</label>
                             </div>
                         </div>
-                        <div className="fs-3">
-                            <input type="radio" className="btn-check" name="options" id="option1" autoComplete="off" checked />
-                            <label htmlFor="option1" className="btn btn-primary btn-lg">Usuario Normal</label>
-                        </div>
-                        <div className="fs-3">
-                            <input type="radio" className="btn-check" name="options" id="option2" autoComplete="off" checked />
-                            <label htmlFor="option2" className="btn btn-primary btn-lg">Clínica Veterinaria</label>
-                        </div>
-                        <div className="fs-3">
-                            <input type="radio" className="btn-check" name="options" id="option3" autoComplete="off" checked />
-                            <label htmlFor="option3" className="btn btn-primary btn-lg">Fundación</label>
-                        </div>
                     </div>
                     <div className="col-12 col-md-7 text-center">
-                        <h2>Registro como {user}</h2>
+                        <h2 className="display-5">Registro como {user}</h2>
                         <div className="border border-secondary p-1 rounded">
                             <div className="text-start">
-                                <span className="input-group-text d-block text-start" htmlFor="name">Nombre </span>
-                                <input className="form-control " type="text" id="name" />
+                                <span className="input-group-text d-block text-start fs-4" htmlFor="tipo">Tipo de Usuario </span>
+                                <input className="form-control fs-5 text-center" type="text" id="tipo" disabled value={user}/>
                             </div>
                             <div className="text-start">
-                                <span className="input-group-text d-block text-start" htmlFor="email">Email </span>
-                                <input className="form-control " type="text" id="email" />
+                                <span className="input-group-text d-block text-start fs-4" htmlFor="name">Nombre </span>
+                                <input className="form-control fs-5" type="text" id="name" />
                             </div>
                             <div className="text-start">
-                                <label className="input-group-text d-block text-start" htmlFor="password">Contraseña <FaEye title={isRevealPwd ? "Hide password":"Show password"} onClick={()=>setIsRevealPwd(prevState =>!prevState)}/></label>
-                                <input className="form-control " type={isRevealPwd ? "text": "password"} id="id_password" placeholder="Ingrese Contraseña"/>
+                                <span className="input-group-text d-block text-start fs-4" htmlFor="lastname">Apellido </span>
+                                <input className="form-control fs-5" type="text" id="lastname" />
+                            </div>
+                            <div className="text-start">
+                                <span className="input-group-text d-block text-start fs-4" htmlFor="email">Email </span>
+                                <input className="form-control fs-5" type="email" id="email" />
+                            </div>
+                            <div className="text-start">
+                                <label className="input-group-text d-block text-start fs-4" htmlFor="password">Contraseña <FaEye title={isRevealPwd ? "Hide password" : "Show password"} onClick={() => setIsRevealPwd(prevState => !prevState)} /></label>
+                                <input className="form-control fs-5" type={isRevealPwd ? "text" : "password"} id="id_password" placeholder="Ingrese Contraseña" />
+                            </div>
+                            <div className="text-start">
+                                <label className="input-group-text d-block text-start fs-4" htmlFor="password">Confirmar Contraseña <FaEye title={isRevealPwdC ? "Hide password" : "Show password"} onClick={() => setIsRevealPwdC(prevState => !prevState)} /></label>
+                                <input className="form-control fs-5" type={isRevealPwdC ? "text" : "password"} id="id_password" placeholder="Confirme Contraseña" />
+                            </div>
+                            <div className="text-start">
+                                <span className="input-group-text d-block text-start fs-4" htmlFor="email">Dirección </span>
+                                <input className="form-control fs-5" type="text" id="direccion" name="direccion"/>
+                            </div>
+                            <div className="text-start">
+                                <span className="input-group-text d-block text-start fs-4" htmlFor="email">Teléfono </span>
+                                <input className="form-control fs-5" type="tel" id="telefono" name="telefono" />
+                            </div>
+                            <div className="text-center">
+                                <button className="btn btn-success mt-3 p-2 fs-4">
+                                    Registrar
+                                </button>
                             </div>
                         </div>
                     </div>
