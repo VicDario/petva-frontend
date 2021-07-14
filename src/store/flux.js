bb096558cd64ab14bfa8c0a5f01ab96ba3135793
zeroSkill <gabbriel.amaya@gmail.com>
2021-07-14 15:54:20 -0400

    login as user

--- src/store/flux.js
+++ src/store/flux.js
@@ -1,31 +1,31 @@
 const getState = ({ getStore, getActions, setStore }) => {
     return {
         store: {
-           
+
 
         },
         actions: {
-           registerClinica : (email,name,address,phone,password)=>{
-            const opt ={
-                method: "POST",
-                body: JSON.stringify({
-                    email: email,
-                    name: name,
-                    address: address,
-                    phone:phone,
-                    password: password
-                }),
-                headers:{
-                    "Content-Type": "application/json"
+            registerClinica: (email, name, address, phone, password) => {
+                const opt = {
+                    method: "POST",
+                    body: JSON.stringify({
+                        email: email,
+                        name: name,
+                        address: address,
+                        phone: phone,
+                        password: password
+                    }),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
                 }
-            }
-                fetch("https://petva-backend-dev.herokuapp.com/api/clinic/register",opt)
-               .then(resp =>resp.json())
-               .then(data =>{
-                   console.log(data)
-               })
-               .catch(error => console.log("Error from loading message from backend", error))
-           },
+                fetch("https://petva-backend-dev.herokuapp.com/api/clinic/register", opt)
+                    .then(resp => resp.json())
+                    .then(data => {
+                        console.log(data)
+                    })
+                    .catch(error => console.log("Error from loading message from backend", error))
+            },
 
             registerUser: (email, name, lastname, password) => {
                 const opt = {
@@ -47,25 +47,42 @@ const getState = ({ getStore, getActions, setStore }) => {
                     })
                     .catch(error => console.log("Error from loading message from backend", error))
             },
-
-           loginClinica : ()=>{
-               const opt = {
-                   method: "POST",
-                   body: JSON.stringify({
-                       email: "d.jacobpulgar@gmail.com",
-                       password: "qwerty123"
-                   }),
-                   headers: {
-                       "Content-Type": "application/json"
-                   }
-               }
-               fetch("https://petva-backend-dev.herokuapp.com/api/clinic/login", opt)
-                   .then(resp => resp.json())
-                   .then(data => {
-                       console.log(data)
-                   })
-                   .catch(error => console.log("Error from loading message from backend", error))
-           }
+            loginUser: (email,password) => {
+                const opt = {
+                    method: "POST",
+                    body: JSON.stringify({
+                        email: email,
+                        password: password
+                    }),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+                fetch("https://petva-backend-dev.herokuapp.com/api/user/login",opt)
+                .then(resp => resp.json())
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(error => console.log("Error from loading message from backend", error))
+            },
+            loginClinica: () => {
+                const opt = {
+                    method: "POST",
+                    body: JSON.stringify({
+                        email: "d.jacobpulgar@gmail.com",
+                        password: "qwerty123"
+                    }),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+                fetch("https://petva-backend-dev.herokuapp.com/api/clinic/login", opt)
+                    .then(resp => resp.json())
+                    .then(data => {
+                        console.log(data)
+                    })
+                    .catch(error => console.log("Error from loading message from backend", error))
+            }
         }
 
 
--- src/views/login.js
+++ src/views/login.js
@@ -1,36 +1,60 @@
-const Login = () => {
+import { useContext, useRef, useState } from "react";
+import { Context } from "../store/appContext";
 
-    const validateForm = (e) => {
-        let password = document.getElementById('password');
-        let password_regex = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
-        if (password.value.trim() === '') {
-            console.error("password empty");
-            return false;
-        } else if (password.value.length < 8) {
-            console.error("password too short");
-            return false;
-        } else if (!password_regex.test(password.value)) {
-            console.error("password may contain at least one mayus letter, one minus letter, one number and one special character.")
+const Login = () => {
+    const { actions } = useContext(Context);
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const inputPassword = useRef();
+    const inputEmail = useRef();
+
+    const validateEmail = (e) => {
+        e.preventDefault();
+        if (inputEmail.current.value.trim() === '') {
+            console.error("Email empty");
             return false;
         } else {
-            console.log(password.value);
+            console.log(email);
+            setEmail(inputEmail.current.value);
             return true;
         }
     }
-
+    const validatePassword = (e) => {
+        e.preventDefault();
+        let password_regex = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+        if (inputPassword.current.value.trim() === '') {
+            console.error("password empty");
+            return false;
+        } else if (inputPassword.current.value.length < 8) {
+            console.error("password too short");
+            return false;
+        } else if (!password_regex.test(inputPassword.current.value)) {
+            console.error("password may contain at least one mayus letter, one minus letter, one number and one special character.")
+            return false;
+        } else {
+            console.log(password);
+            setPassword(inputPassword.current.value);
+            return true;
+        }
+    }
+    const handleSubmit = (e) => {
+        if (validatePassword(e) && validateEmail(e)) {
+            actions.loginUser(email, password);
+        }
+    }
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-6 col-sm-10 mx-auto my-3 text-center">
                     <main className="form-sigin bg-white rounded-3 p-4">
-                        <form className="px-3" onSubmit={(e) => !validateForm(e) ? e.preventDefault() : ""}>
+                        <form className="px-3" onSubmit={(e) => handleSubmit(e)}>
                             <h1 className="h2 mb-4">Inicio de Sesión</h1>
                             <div className="form-floating my-3 w-80">
-                                <input type="email" className="form-control" id="email" placeholder="name@example.com" />
+                                <input type="email" ref={inputEmail} onChange={(e) => validateEmail(e)} className="form-control" id="email" placeholder="name@example.com" />
                                 <label htmlFor="email">Email</label>
                             </div>
                             <div className="form-floating mt-3 mb-2">
-                                <input type="password" className="form-control" placeholder="********" id="password" />
+                                <input type="password" ref={inputPassword} onChange={(e) => validatePassword(e)} className="form-control" placeholder="********" id="password" />
                                 <label htmlFor="password">Password</label>
                             </div>
                             <div className="checkbox mb-2">
