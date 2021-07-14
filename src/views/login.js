92b2bddd4d1b119743d52cadd716a85aa2828fa0
zeroSkill <gabbriel.amaya@gmail.com>
2021-07-13 20:03:08 -0400

    validation of password done

--- src/views/login.js
+++ src/views/login.js
@@ -1,10 +1,29 @@
 const Login = () => {
+
+    const validateForm = (e) => {
+        let password = document.getElementById('password');
+        let password_regex = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+        if (password.value.trim() === '') {
+            console.error("password empty");
+            return false;
+        } else if (password.value.length < 8) {
+            console.error("password too short");
+            return false;
+        } else if (!password_regex.test(password.value)) {
+            console.error("password may contain at least one mayus letter, one minus letter, one number and one special character.")
+            return false;
+        } else {
+            console.log(password.value);
+            return true;
+        }
+    }
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-6 col-sm-10 mx-auto my-3 text-center">
                     <main className="form-sigin bg-white rounded-3 p-4">
-                        <form className="px-3">
+                        <form className="px-3" onSubmit={(e) => !validateForm(e) ? e.preventDefault() : ""}>
                             <h1 className="h2 mb-4">Inicio de Sesión</h1>
                             <div className="form-floating my-3 w-80">
                                 <input type="email" className="form-control" id="email" placeholder="name@example.com" />
