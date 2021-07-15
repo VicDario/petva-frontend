23e435dcfeab894d4bbe11c8a08e7e02533e4ee2
zeroSkill <gabbriel.amaya@gmail.com>
2021-07-15 13:06:05 -0400

    view fundation profile

---
+++ src/views/fundationprofile.js
@@ -0,0 +1,48 @@
+import { useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+const FundationProfile = () => {
+    const { store } = useContext(Context);
+    let { token } = store;
+    const history = useHistory();
+
+    return (
+        <>
+            {
+                token !== "" ? (
+                    <div className="container">
+                        <div className="text-center my-4">
+                            <h2 className="display-1">Bienvenido Fundación</h2>
+                        </div>
+                        <div className="row">
+                            <div className="col-12 col-md-6 text-center">
+                                <button className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold">
+                                    Mis Datos
+                                </button>
+                            </div>
+                            <div className="col-12 col-md-6 text-center">
+                                <button className="btn btn-secondary btn-lg fs-2 my-3  item texto-borde fw-bold ">
+                                    Perdidos y Encontrados
+                                </button>
+                            </div>
+                            <div className="col-12 col-md-6 d-flex justify-content-center">
+                                <Link to="/fundation/pets" className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold d-flex align-items-center justify-content-center ">
+                                    Mis Mascotas
+                                </Link >
+                            </div>
+                            <div className="col-12 col-md-6 text-center">
+                                <button className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold">
+                                    Servicios
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    history.push("/fundation/login")
+                )
+            }
+        </>
+    );
+}
+export default FundationProfile;
