63868ec10e5230e0c531e7cdcddfa637b387e2a5
zeroSkill <gabbriel.amaya@gmail.com>
2021-07-17 13:19:53 -0400

    added         // eslint-disable-next-line react-hooks/exhaustive-deps

--- src/views/fundationpets.js
+++ src/views/fundationpets.js
@@ -9,7 +9,8 @@ const Fundationpets = () => {
     //let {pets} =store;
     let token = localStorage.getItem('token');
     useEffect(() => {
-        actions.getPetsFundation()
+        actions.getPetsFundation();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
         <>
@@ -23,38 +24,38 @@ const Fundationpets = () => {
                                 <h2 className="display-1">Mascotas de la Fundación</h2>
                             </div>
                             <div className="row justify-content-center">
-                                
-                            {
-                                !!store.pets ?
-                                 store.pets.length > 0 ?
-                                store.pets.map((pet, index) => {
-                                    return (
-                                        <div className="col-sm-6 col-md-4" key={index}>
-                                            <div className="card mb-3">
-                                                <img src={!!pet.picture ? pet.picture : "/images/default.jpg"} className="card-img-top" alt={pet.name} />
-                                                <div className="card-body">
-                                                    <h5 className="card-title">{pet.name}</h5>
-                                                    <p className="card-text">{pet.specie === 'cat' ? "Gato" : "Perro"}</p>
-                                                    <p className="card-text">{!!pet.birth_date ? pet.birth_date : "No registra fecha de nacimiento"}</p>
-                                                    <p className="card-text">{!!pet.chip_code ? pet.code_chip : "No registra codigo de chip"}</p>
-                                                    <p className="card-text badge rounded-pill bg-success fs-3">{pet.state==="owned"?"Con Dueño":"En adopción"}</p>
-                                                    
-                                                    <div className="d-flex justify-content-around">
-                                                    <Link href="/infomascota" className="btn btn-primary">Detalles</Link>
-                                                    <Link to={"/foundation/transfer/" +pet.id} className="btn btn-danger">Transferir</Link>
+
+                                {
+                                    !!store.pets ?
+                                        store.pets.length > 0 ?
+                                            store.pets.map((pet, index) => {
+                                                return (
+                                                    <div className="col-sm-6 col-md-4" key={index}>
+                                                        <div className="card mb-3">
+                                                            <img src={!!pet.picture ? pet.picture : "/images/default.jpg"} className="card-img-top" alt={pet.name} />
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">{pet.name}</h5>
+                                                                <p className="card-text">{pet.specie === 'cat' ? "Gato" : "Perro"}</p>
+                                                                <p className="card-text">{!!pet.birth_date ? pet.birth_date : "No registra fecha de nacimiento"}</p>
+                                                                <p className="card-text">{!!pet.chip_code ? pet.code_chip : "No registra codigo de chip"}</p>
+                                                                <p className="card-text badge rounded-pill bg-success fs-3">{pet.state === "owned" ? "Con Dueño" : "En adopción"}</p>
+
+                                                                <div className="d-flex justify-content-around">
+                                                                    <Link href="/infomascota" className="btn btn-primary">Detalles</Link>
+                                                                    <Link to={"/foundation/transfer/" + pet.id} className="btn btn-danger">Transferir</Link>
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     </div>
-                                                </div>
+                                                )
+                                            })
+                                            :
+                                            <div className="col-sm-12 pt-4">
+                                                <h3 className="text-center">No tienes Mascotas registradas</h3>
                                             </div>
-                                        </div>
-                                    )
-                                })
-                                :
-                                        <div className="col-sm-12 pt-4">
-                                            <h3 className="text-center">No tienes Mascotas registradas</h3>
-                                        </div>
                                         :
-                                        <LoadingSpiner/>
-                            }
+                                        <LoadingSpiner />
+                                }
                             </div>
                             <div>
                                 <Link to="/addpetuser" className="text-decoration-none badge rounded-pill bg-success p-3 m-1 fs-4">
@@ -67,7 +68,7 @@ const Fundationpets = () => {
             {
                 !store.token &&
                 history.push("/")
-                
+
             }
         </>
     );
