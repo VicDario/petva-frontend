0af9d82a1c0f98c00b82ba5c23473e596eee650e
zeroSkill <gabbriel.amaya@gmail.com>
2021-07-17 22:57:02 -0400

    se ve detalle de fundacion

--- src/views/fundationprofiledetail.js
+++ src/views/fundationprofiledetail.js
@@ -2,12 +2,12 @@ import { useContext } from "react";
 import { Context } from "../store/appContext";
 import { useEffect } from "react";
 
-const Fundationprofiledetail = () => {
+const Foundationprofiledetail = () => {
     const { actions, store } = useContext(Context);
 
     useEffect(() => {
-        //deberia estar el get mascotas fundation
-        actions.getFundationDetail();
+        //deberia estar el get mascotas foundation
+        actions.getFoundationDetail();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
@@ -21,24 +21,24 @@ const Fundationprofiledetail = () => {
                     <div>
 
                         {
-                            !!store.fundationDetail &&
+                            !!store.foundationDetail &&
 
                             <div className="card mb-3" style={{ maxWidth: 540 }}>
                                 <div className="row g-0">
                                     <div className="col-md-4">
-                                        <img src={store.fundationDetail.picture} className="img-fluid rounded-start" alt="Imagen Fundación" />
+                                        <img src={store.foundationDetail.picture} className="img-fluid rounded-start" alt="Imagen Fundación" />
                                     </div>
                                     <div className="col-md-8">
                                         <div className="card-body">
-                                            <h5 className="card-title">{store.fundationDetail.name}</h5>
+                                            <h5 className="card-title">{store.foundationDetail.name}</h5>
                                             <p className="card-text">
-                                                {store.fundationDetail.email}
+                                                {store.foundationDetail.email}
                                             </p>
                                             <p className="card-text">
-                                                {store.fundationDetail.phone}
+                                                {store.foundationDetail.phone}
                                             </p>
                                             <p className="card-text">
-                                                {store.fundationDetail.address}
+                                                {store.foundationDetail.address}
                                             </p>
                                             <p className="card-text">
                                                 <small className="text-muted">Numero de Mascotas actuales: </small>
@@ -55,4 +55,4 @@ const Fundationprofiledetail = () => {
         </>
     )
 }
-export default Fundationprofiledetail;
+export default Foundationprofiledetail;
