89df1334d37b921ad1aed6c7fefd580697bca3c0
Victor Fernandez <26585356+VicDario@users.noreply.github.com>
2021-07-21 13:12:49 -0400

    Update userpethistory.js

--- src/views/userpethistory.js
+++ src/views/userpethistory.js
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
-import { FaPlusCircle } from "react-icons/fa"
+import { FaPlusCircle, FaTrash } from "react-icons/fa"
 import { FaCat, FaDog } from "react-icons/fa";
 
 const Userpethistory = () => {
