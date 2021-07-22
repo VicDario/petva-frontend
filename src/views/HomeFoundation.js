e0ead1b71255c9bf130acdfe26f0c63a4e8a274d
zero-skill <gabbriel.amaya@gmail.com>
2021-07-22 17:00:16 -0400

    fixing some bugcitos

--- src/views/HomeFoundation.js
+++ src/views/HomeFoundation.js
@@ -9,44 +9,44 @@ const HomeFoundation = () => {
 
     return (
         <>
-        {
-            !!localStorage.getItem("token") !== null ? 
-                (
-                    <div className="container">
-                        <div className="text-center my-4">
-                            <h2 className="display-1">Bienvenido Fundación</h2>
+            {
+                localStorage.getItem("petvaToken") !== null ?
+                    (
+                        <div className="container">
+                            <div className="text-center my-4">
+                                <h2 className="display-1">Bienvenido Fundación</h2>
+                            </div>
+                            <div className="row">
+                                <div className="col-12 col-md-6 d-flex justify-content-center">
+                                    <Link to="/foundation/profile" className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold d-flex align-items-center justify-content-center ">
+                                        Mis Datos
+                                    </Link >
+                                </div>
+                                <div className="col-12 col-md-6 text-center">
+                                    <button className="btn btn-secondary btn-lg fs-2 my-3  item texto-borde fw-bold ">
+                                        Perdidos y Encontrados
+                                    </button>
+                                </div>
+                                <div className="col-12 col-md-6 d-flex justify-content-center">
+                                    <Link to="/foundation/pets/adoption" className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold d-flex align-items-center justify-content-center ">
+                                        Mascotas en Adopción
+                                    </Link >
+                                </div>
+                                <div className="col-12 col-md-6 d-flex justify-content-center">
+                                    <Link to="/foundation/pets/tracking" className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold d-flex align-items-center justify-content-center ">
+                                        Mascotas Adoptadas o con dueño
+                                    </Link >
+                                </div>
+                                <div className="col-12 col-md-6 d-flex justify-content-center">
+                                    <Link to="/services" className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold d-flex align-items-center justify-content-center ">
+                                        Servicios
+                                    </Link >
+                                </div>
+                            </div>
                         </div>
-                        <div className="row">
-                            <div className="col-12 col-md-6 d-flex justify-content-center">
-                                <Link to="/foundation/profile" className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold d-flex align-items-center justify-content-center ">
-                                    Mis Datos
-                                </Link >
-                            </div>
-                            <div className="col-12 col-md-6 text-center">
-                                <button className="btn btn-secondary btn-lg fs-2 my-3  item texto-borde fw-bold ">
-                                    Perdidos y Encontrados
-                                </button>
-                            </div>
-                            <div className="col-12 col-md-6 d-flex justify-content-center">
-                                <Link to="/foundation/pets/adoption" className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold d-flex align-items-center justify-content-center ">
-                                    Mascotas en Adopción
-                                </Link >
-                            </div>
-                            <div className="col-12 col-md-6 d-flex justify-content-center">
-                                <Link to="/foundation/pets/tracking" className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold d-flex align-items-center justify-content-center ">
-                                    Mascotas Adoptadas o con dueño
-                                </Link >
-                            </div>
-                            <div className="col-12 col-md-6 d-flex justify-content-center">
-                                <Link to="/services" className="btn btn-secondary btn-lg fs-2 my-3 item texto-borde fw-bold d-flex align-items-center justify-content-center ">
-                                    Servicios
-                                </Link >
-                            </div>
-                        </div>
-                    </div>
-                ) 
-            : 
-                history.push("/foundation/login")
+                    )
+                    :
+                    history.push("/foundation/login")
             }
         </>
     );
--- src/views/HomeUser.js
+++ src/views/HomeUser.js
@@ -1,36 +1,29 @@
-
 import { Link, useHistory } from "react-router-dom";
-import {FcOvertime} from "react-icons/fc"
- 
-
-
+import { FcOvertime } from "react-icons/fc"
 const HomeUser = () => {
-    
-    
     const history = useHistory()
-
     return (
-        <> 
-        {
-            !!localStorage.getItem("petvatoken") !== null ?
-                <div className="container">
-                    <div className="text-center my-4">
-                        <h2 className="display-1">Bienvenido Usuario</h2>
-                    </div>
-                    <div className="row">
-                        <div className="col-12 col-md-6 d-flex justify-content-center">
-                            <Link to="/user/reserve" 
-                            className="btn btn-primary fs-2 fw-bold ">
-                                <span className="fs-1 m-0"><FcOvertime/></span>
-                                <p>Reservar</p>
-                                <p>Hora Veterinaria</p>
-                            </Link >
+        <>
+            {
+                localStorage.getItem("petvaToken") !== null ?
+                    <div className="container">
+                        <div className="text-center my-4">
+                            <h2 className="display-1">Bienvenido Usuario</h2>
                         </div>
-                       
+                        <div className="row">
+                            <div className="col-12 col-md-6 d-flex justify-content-center">
+                                <Link to="/user/reserve"
+                                    className="btn btn-primary fs-2 fw-bold ">
+                                    <span className="fs-1 m-0"><FcOvertime /></span>
+                                    <p>Reservar</p>
+                                    <p>Hora Veterinaria</p>
+                                </Link >
+                            </div>
+
+                        </div>
                     </div>
-                </div>
-            :
-                history.push("/user/login")
+                    :
+                    history.push("/user/login")
             }
         </>
     )
