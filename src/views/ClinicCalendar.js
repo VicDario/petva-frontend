0eee91fe5067ba6f0c27a7299c0fa8f535fa7cf2
VicDario <26585356+VicDario@users.noreply.github.com>
2021-07-23 11:42:25 -0400

    Calendar and some styles

--- src/views/ClinicCalendar.js
+++ src/views/ClinicCalendar.js
@@ -4,6 +4,7 @@ import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import moment from 'moment';
+import { IoHelpCircle } from "react-icons/io5";
 
 const ClinicCalendar = (props) => {
     const { actions } = useContext(Context);
@@ -25,14 +26,19 @@ const ClinicCalendar = (props) => {
             let title = "";
             let backgroundColor = "";
             if (events[i].id_pet === null){
-                title=`${events[i].doctor_name} Disponible`;
+                title=`Dr. ${events[i].doctor_name} Disponible`;
                 backgroundColor = "#4287f5";
             }else{
-                title = `${events[i].name_doctor}, Paciente: ${events[i].info_pet.name}, Email: ${events[i].email}`;
+                title = `Dr. ${events[i].doctor_name}, Paciente: ${events[i].info_pet.name}, Email: ${events[i].email_customer}`;
                 backgroundColor = "#f5ce42";
             }
-            let start = moment(events[i].date_start).format();
-            let end = moment(events[i].end_time).format();
+            let start = moment.parseZone(events[i].date_start).format();
+            let end = moment.parseZone(events[i].date_end).format();
+            
+            /* console.log(start);
+            console.log(events[i].date_end)
+            console.log(end);
+            console.log(events[i].date_start) */
             api.addEvent({
                 title,
                 start,
@@ -43,37 +49,81 @@ const ClinicCalendar = (props) => {
             })
         }
     }
+
+    const handleEventClick = (arg) => {
+        console.log(arg);
+    }
     useEffect(() => {
-        syncEvens();
+        let api = calendar.current.getApi();
+        api.removeAllEvents();
+        syncEvens()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
-        <FullCalendar
-        ref={calendar}
-        plugins={[ timeGridPlugin, interactionPlugin ]}
-        initialView="timeGridDay"
-        allDaySlot={false}
-        businessHours={{
-            daysOfWeek: [ 1, 2, 3, 4, 5, 6, 7 ],
-            startTime: '08:00', 
-            endTime: '20:00' 
-        }
-        }
-        slotDuration={"00:30"}
-        dateClick={handleDateClick}
-        height={"85vh"}
-        validRange= {function (){
-                const today = new Date();
-                const day = today.getDate();
-                const month = today.getMonth() + 1;
-                const year = today.getFullYear(); 
-                return {
-                    start: `${year}-${month > 9 ? month : `0${month}`}-${day}`
-                }
-            }
-        }
-        />
+        <>
+            <FullCalendar
+                timeZone= 'America/Santiago'
+                ref={calendar}
+                plugins={[ timeGridPlugin, interactionPlugin ]}
+                headerToolbar= {{
+                    left: 'prev,next addEventButton',
+                    center: 'title',
+                    right: 'today timeGridDay,timeGridWeek'
+                }}
+                initialView="timeGridDay"
+                allDaySlot={false}
+                businessHours={{
+                    daysOfWeek: [ 1, 2, 3, 4, 5, 6, 7 ],
+                    startTime: '08:00',
+                    endTime: '20:00' 
+                }}
+                slotDuration={"00:30"}
+                dateClick={handleDateClick}
+                eventClick={handleEventClick}
+                height={"84vh"}
+                validRange= {function (){
+                        const today = new Date();
+                        const day = today.getDate();
+                        const month = today.getMonth() + 1;
+                        const year = today.getFullYear(); 
+                        return {
+                            start: `${year}-${month > 9 ? month : `0${month}`}-${day}`
+                        }
+                }}
+                customButtons={{
+                    addEventButton: {
+                    text: 'Actualizar Calendario',
+                    click: function() {
+                        let api = calendar.current.getApi();
+                        api.removeAllEvents();
+                        syncEvens()
+                    }
+                }}}
+            />
+
+            <button type="button" className="btn btn-primary fixed" data-bs-toggle="modal" data-bs-target="#modalHelpCalendarClinic">
+            <IoHelpCircle className="btn--help" />
+            </button>
+
+            <div className="modal fade" id="modalHelpCalendarClinic" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Ayuda</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    - Seleccionar una fecha para ver las horas reservadas y las horas disponibles<br/>
+                    - Puede borrar consultas. Recomendable comunicarse con el cliente antes de borrar la hora.
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+                </div>
+            </div>
+            </div>
+        </>
     )
 }
 
