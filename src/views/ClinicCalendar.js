f8b7266ccae3c2bfc5f774256be1f35a9f284b22
VicDario <26585356+VicDario@users.noreply.github.com>
2021-07-24 18:47:11 -0400

    Modal deleted

--- src/views/ClinicCalendar.js
+++ src/views/ClinicCalendar.js
@@ -6,7 +6,6 @@ import interactionPlugin from "@fullcalendar/interaction";
 import moment from 'moment';
 import { IoHelpCircle } from "react-icons/io5";
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
-import Modal from '../Components/Modal'
 
 const ClinicCalendar = (props) => {
     const { actions } = useContext(Context);
@@ -77,7 +76,7 @@ const ClinicCalendar = (props) => {
                 headerToolbar= {{
                     left: 'prev,next addEventButton',
                     center: 'title',
-                    right: 'today timeGridDay,timeGridWeek, timeGridMonth'
+                    right: 'today timeGridDay,timeGridWeek'
                 }}
                 initialView="timeGridDay"
                 allDaySlot={false}
@@ -121,11 +120,59 @@ const ClinicCalendar = (props) => {
                     </div>
                 </div>
             </div>
-            
-            <Modal status='reserved' title={title}/>
-            <Modal status='available' title={title}/>
-            <Modal status='canceled' title={title}/>
-            
+            <div className="modal fade" id="modalAvailable" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Hora Disponible</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        {title}
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-warning" data-bs-dismiss="modal">Perdida</button>
+                        <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal fade" id="modalCanceled" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Hora Cancelada</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        {title}
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-success" data-bs-dismiss="modal">Disponible</button>
+                        <button type="button" className="btn btn-warning" data-bs-dismiss="modal">Perdida</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal fade" id="modalReserved" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Hora Reservada</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        {title}
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-success" data-bs-dismiss="modal">Confirmar Asistencia</button>
+                        <button type="button" className="btn btn-warning" data-bs-dismiss="modal">Perdida</button>
+                        <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        )
         </>
     )
 }
