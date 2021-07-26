db7b1b53d2dbd6eed68db7d96fe607f996d42677
VicDario <26585356+VicDario@users.noreply.github.com>
2021-07-26 17:18:45 -0400

    Changes in titles

--- src/views/ClinicCalendar.js
+++ src/views/ClinicCalendar.js
@@ -38,13 +38,13 @@ const ClinicCalendar = (props) => {
                 }
                 backgroundColor = "#db4b3d";
             }else if(events[i].status === 'confirmed'){
-                title = `Dr. ${events[i].doctor_name}, Paciente: ${events[i].info_pet.name} Confirmada`;
+                title = `Dr. ${events[i].doctor_name}, Paciente: ${events[i].info_pet.name}. Cita confirmada`;
                 backgroundColor = "#359c33";
             }else if(events[i].status === 'finished'){
-                title = `Cita de Dr. ${events[i].doctor_name} Finalizada`;
+                title = `Cita de Dr. ${events[i].doctor_name}. Atendida`;
                 backgroundColor = "#2444b5";
             }else if(events[i].status === 'missed'){
-                title = `Cita de Dr. ${events[i].doctor_name} Perdida`;
+                title = `Cita de Dr. ${events[i].doctor_name}. Perdida`;
                 backgroundColor = "#c24725";
             }
             let start = moment.parseZone(events[i].date_start).format();
