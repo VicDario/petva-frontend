217eb954aca3fd3ba4a488884bec61e959327746
Diego <d.jacobpulgar@gmail.com>
2021-07-26 17:17:59 -0400

    user can't add vaccines, diagnostics and surgeries

--- src/views/UserPetHistory.js
+++ src/views/UserPetHistory.js
@@ -15,22 +15,6 @@ const UserPetHistory = () => {
     const [last_location, setLastLocation] = useState();
     const history = useHistory();
 
-    const [newVaccine, setNewVaccine] = useState({
-        date: null,
-        lot: null,
-        name: null,
-        laboratory: null
-    });
-    const [newDiagnostic, setNewDiagnostic] = useState({
-        date: null,
-        diagnostic: null,
-        doctor_name: null
-    })
-    const [newSurgery, setNewSurgery] = useState({
-        date: null,
-        description: null,
-        doctor_name: null
-    })
     const formatDate = (date) => {
         let newdate = date.split("-")
         newdate = newdate.reverse()
@@ -61,57 +45,7 @@ const UserPetHistory = () => {
         actions.getHistoryUserPet(pet_id);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    const addVaccine = () => {
-        if (newVaccine.date !== null && newVaccine.lot !== null && newVaccine.name !== null && newVaccine.laboratory !== null)
-        {
-            actions.addVaccinetoPetUser(formatDate(newVaccine.date), newVaccine.lot, newVaccine.name, newVaccine.laboratory, pet_id)
-            console.log("Vacuna agregada");
-            setNewVaccine({
-                date: null,
-                lot: null,
-                name: null,
-                laboratory: null
-            })
-            /* actions.getHistoryUserPet(pet_id); */
-        } else
-        {
-            console.log("Vacuna no agregada");
-            alert("Debes llenar todos los campos para agregar vacuna");
-        }
-    }
-    const addDiagnostic = () => {
-        if (newDiagnostic.date !== null && newDiagnostic.diagnostic !== null && newDiagnostic.doctor_name !== null)
-        {
-            actions.addDiagnostictoPetUser(formatDate(newDiagnostic.date), newDiagnostic.diagnostic, newDiagnostic.doctor_name, pet_id)
-            console.log("Diagnóstico agregado");
-            setNewDiagnostic({
-                date: null,
-                diagnostic: null,
-                doctor_name: null
-            })
-        } else
-        {
-            console.log("diagnóstico no agregado");
-            alert("Debes llenar todos los campos para agregar diagnóstico");
-        }
-    }
-
-    const addSurgery = () => {
-        if (newSurgery.date !== null && newSurgery.description !== null && newSurgery.doctor_name !== null)
-        {
-            actions.addSurgerytoPetUser(formatDate(newSurgery.date), newSurgery.description, newSurgery.doctor_name, pet_id)
-            console.log("Cirugia agregada");
-            setNewSurgery({
-                date: null,
-                description: null,
-                doctor_name: null
-            })
-        } else
-        {
-            console.log("cirugía no agregada");
-            alert("Debes llenar todos los campos para agregar cirugía");
-        }
-    }
+    
     const reportLost = () => {
         actions.userReportPetLost(pet_id, last_location);
         console.log(last_location)
@@ -333,10 +267,8 @@ const UserPetHistory = () => {
                             <span
                                 title="Agregar Vacuna"
                                 className="text-success"
-                                type="button"
-                                data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">
-                                <FaPlusCircle />
+                                >
+                                
                             </span>
                         </h2>
                     </div>
@@ -412,10 +344,7 @@ const UserPetHistory = () => {
                             <span
                                 title="Agregar consulta"
                                 className="text-success"
-                                type="button"
-                                data-bs-toggle="modal"
-                                data-bs-target="#diagnostico">
-                                <FaPlusCircle />
+                                >
                             </span>
                         </h2>
                     </div>
@@ -494,10 +423,7 @@ const UserPetHistory = () => {
                         className="text-center">
                         <h2>Cirugías <span
                             title="Agregar Cirugía"
-                            className="text-success" type="button"
-                            data-bs-toggle="modal"
-                            data-bs-target="#cirugia">
-                            <FaPlusCircle />
+                            className="text-success">
                         </span>
                         </h2>
                     </div>
@@ -569,264 +495,6 @@ const UserPetHistory = () => {
                     }
                 </div>
 
-            </div>
-            {/* Modal  agregar Vacuna*/}
-            <div
-                className="modal fade"
-                id="exampleModal"
-                tabIndex={-1}
-                aria-labelledby="exampleModalLabel"
-                aria-hidden="true"
-            >
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">
-                                Agregar Vacuna
-                            </h5>
-                            <button
-                                type="button"
-                                className="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                            />
-                        </div>
-                        <div className="modal-body">
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="dateV">Fecha Vacuna:
-                                </label>
-                                <input
-                                    className="form-control"
-                                    id="dateV"
-                                    type="date"
-                                    onChange={(e) => { setNewVaccine({ ...newVaccine, date: e.target.value }) }} />
-                            </div>
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="lotV">
-                                    Lote o Número de Serie:
-                                </label>
-                                <input
-                                    className="form-control"
-                                    id="lotV" type="text"
-                                    onChange={(e) => { setNewVaccine({ ...newVaccine, lot: e.target.value }) }} />
-                            </div>
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="nameV">Nombre Vacuna: </label>
-                                <input
-                                    className="form-control"
-                                    id="nameV" type="text"
-                                    onChange={(e) => { setNewVaccine({ ...newVaccine, name: e.target.value }) }} />
-                            </div>
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="laboratoryV">Laboratorio: </label>
-                                <input
-                                    className="form-control"
-                                    id="laboratoryV"
-                                    type="text"
-                                    onChange={(e) => { setNewVaccine({ ...newVaccine, laboratory: e.target.value }) }} />
-                            </div>
-                        </div>
-                        <div
-                            className="modal-footer">
-                            <button
-                                type="button"
-                                className="btn btn-secondary"
-                                data-bs-dismiss="modal"
-                            >
-                                Cancelar
-                            </button>
-                            <button
-                                onClick={addVaccine}
-                                type="button"
-                                className="btn btn-primary"
-                                data-bs-dismiss={newVaccine.date !== null &&
-                                    newVaccine.lot !== null &&
-                                    newVaccine.name !== null
-                                    && newVaccine.laboratory !== null ? "modal" : ""}>
-                                Agregar
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* Modal agregar Diagnostico */}
-            <div
-                className="modal fade"
-                id="diagnostico"
-                tabIndex={-1}
-                aria-labelledby="diagnosticoLabel"
-                aria-hidden="true"
-            >
-                <div
-                    className="modal-dialog">
-                    <div
-                        className="modal-content">
-                        <div
-                            className="modal-header">
-                            <h5
-                                className="modal-title" id="diagnosticoLabel">
-                                Agregar Consulta
-                            </h5>
-                            <button
-                                type="button"
-                                className="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                            />
-                        </div>
-                        <div
-                            className="modal-body">
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="dateD">Fecha Consulta: </label>
-                                <input
-                                    className="form-control"
-                                    id="dateD"
-                                    type="date"
-                                    onChange={(e) => { setNewDiagnostic({ ...newDiagnostic, date: e.target.value }) }} />
-                            </div>
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="diagD">Diagnóstico: </label>
-                                <input
-                                    className="form-control"
-                                    id="diagD"
-                                    type="text"
-                                    onChange={(e) => { setNewDiagnostic({ ...newDiagnostic, diagnostic: e.target.value }) }}
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="dnD">Nombre Doctor:
-                                </label>
-                                <input
-                                    className="form-control"
-                                    id="dnD"
-                                    type="text"
-                                    onChange={(e) => { setNewDiagnostic({ ...newDiagnostic, doctor_name: e.target.value }) }} />
-                            </div>
-                        </div>
-                        <div
-                            className="modal-footer">
-                            <button
-                                type="button"
-                                className="btn btn-secondary"
-                                data-bs-dismiss="modal"
-                            >
-                                Cancelar
-                            </button>
-                            <button
-                                onClick={addDiagnostic}
-                                type="button"
-                                className="btn btn-primary"
-                                data-bs-dismiss={newDiagnostic.date !== null &&
-                                    newDiagnostic.diagnostic !== null &&
-                                    newDiagnostic.doctor_name !== null ? "modal"
-                                    : ""}>
-                                Agregar
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* Modal agregar Cirugía */}
-            <div
-                className="modal fade"
-                id="cirugia"
-                tabIndex={-1}
-                aria-labelledby="cirugiaLabel"
-                aria-hidden="true"
-            >
-                <div
-                    className="modal-dialog">
-                    <div
-                        className="modal-content">
-                        <div
-                            className="modal-header">
-                            <h5
-                                className="modal-title"
-                                id="cirugiaLabel">
-                                Agregar Cirugía
-                            </h5>
-                            <button
-                                type="button"
-                                className="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                            />
-                        </div>
-                        <div
-                            className="modal-body">
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="dateC">
-                                    Fecha Cirugía:
-                                </label>
-                                <input
-                                    className="form-control"
-                                    id="dateC"
-                                    type="date"
-                                    onChange={(e) => { setNewSurgery({ ...newSurgery, date: e.target.value }) }}
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="cirD">Descripción: </label>
-                                <input
-                                    className="form-control"
-                                    id="cirD"
-                                    type="text"
-                                    onChange={(e) => { setNewSurgery({ ...newSurgery, description: e.target.value }) }}
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    className="form-label"
-                                    htmlFor="cirDn">
-                                    Nombre Doctor:
-                                </label>
-                                <input
-                                    className="form-control"
-                                    id="cirDn"
-                                    type="text"
-                                    onChange={(e) => { setNewSurgery({ ...newSurgery, doctor_name: e.target.value }) }}
-                                />
-                            </div>
-                        </div>
-                        <div className="modal-footer">
-                            <button
-                                type="button"
-                                className="btn btn-secondary"
-                                data-bs-dismiss="modal"
-                            >
-                                Cancelar
-                            </button>
-                            <button
-                                onClick={addSurgery}
-                                type="button"
-                                className="btn btn-primary"
-                                data-bs-dismiss={newSurgery.date !== null &&
-                                    newSurgery.description !== null &&
-                                    newSurgery.doctor_name !== null ?
-                                    "modal" : ""}>
-                                Agregar
-                            </button>
-                        </div>
-                    </div>
-                </div>
             </div>
             {/* Modal Para mascota perdida */}
             <div
