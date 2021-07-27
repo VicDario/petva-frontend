786d3b50698c2ba7c1daff5e82b1454119b844eb
zero-skill <gabbriel.amaya@gmail.com>
2021-07-27 19:06:02 -0400

    login stylized

--- src/views/Login.js
+++ src/views/Login.js
@@ -38,17 +38,17 @@ const Login = () => {
         }
     }
     return (
-        <div className="container">
+        <div className="container container-login">
             <div className="row">
                 <div className="col-md-6 col-sm-10 mx-auto my-3 text-center">
-                    <main className="form-sigin bg-white rounded-3 p-4">
+                    <main className="form-sigin bg-login p-4 mt-3">
                         <form className="px-3" onSubmit={handleSubmit}>
                             <h1 className="h2 mb-4">Inicio de Sesión</h1>
-                            <div className="form-floating my-3 w-80">
+                            <div className="form-floating login-input my-3 w-80">
                                 <input type="email" ref={inputEmail} onChange={(e) => validateEmail(e)} className="form-control" id="email" placeholder="name@example.com" />
                                 <label htmlFor="email">Email</label>
                             </div>
-                            <div className="form-floating mt-3 mb-2">
+                            <div className="form-floating login-input my-4 ">
                                 <input type="password" ref={inputPassword} className="form-control" placeholder="********" id="password" />
                                 <label htmlFor="password">Password</label>
                             </div>
@@ -58,7 +58,7 @@ const Login = () => {
                                     Recuerdame
                                 </label>
                             </div>*/}
-                            <button className="w-50 btn btn-primary mt-3" type="submit">Iniciar Sesión</button>
+                            <button className="btn btn-login mt-3 px-5" type="submit">Iniciar Sesión</button>
                             {/*<div className="mt-1">
                                 <span className="text-muted">¿Has olvidado tu contraseña?</span>
                             </div>*/}
