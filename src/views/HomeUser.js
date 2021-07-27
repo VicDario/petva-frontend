f0fd13936cc874c6dbe08028de893aeb3dd43d71
VicDario <26585356+VicDario@users.noreply.github.com>
2021-07-27 19:59:23 -0400

    homeuser

--- src/views/HomeUser.js
+++ src/views/HomeUser.js
@@ -10,41 +10,52 @@ const HomeUser = () => {
     useEffect(() => {
         actions.getUserDetail();
         actions.getMascotasUser();
+        if (localStorage.getItem("petvaUser") !== 'normal') {
+            history.push("/user/login");
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return (
         <>
             {
                 !!localStorage.getItem("petvaToken") ?
-                    <div className="container">
+                    <div className="container-fluid">
                         {
                             !!store.userDetail &&
-                            <div className="text-center my-4">
-                                <h2 className="display-1">Bienvenido {store.userDetail.name}</h2>
+                            <div className="row">
+                                <div className="col-md-6 my-1 d-flex justify-content-center ps-5">
+                                    <div className="my-4">
+                                        <h2 className="fw-bold title-home">Bienvenido, {store.userDetail.name}</h2>
+                                    </div>
+                                </div>
                             </div>
                         }
                         {
                             !!store.pets ?
                                 store.pets.length > 0 ?
+                                    <>
                                     <div className="row">
-                                        <div className="col-12 col-md-6 my-1 d-flex justify-content-center">
+                                        <div className="col-md-6 my-1 d-flex justify-content-center pb-3">
                                             <Link to="/user/reserve"
-                                                className="btn btn-home-user fs-2 fw-bold ">
+                                                className="link-green btn-home">
                                                 <span className="fs-1 m-0"><FcOvertime /></span>
                                                 <p>Reservar</p>
                                                 <p>Hora Veterinaria</p>
                                             </Link >
                                         </div>
-                                        <div className="col-12 col-md-6 my-1 d-flex justify-content-center">
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-6 my-1 d-flex justify-content-center">
                                             <Link to="/user/reservations"
-                                                className="btn btn-home-user fs-2 fw-bold ">
+                                                className="link-green btn-home">
                                                 <span className="fs-1 m-0"><FcOvertime /></span>
                                                 <p>Ver y editar</p>
                                                 <p>mis horas veterinarias</p>
                                             </Link >
                                         </div>
                                     </div>
-                                    :
+                                    </>
+                                :
                                     <div
                                         className="col-sm-12 py-4 d-flex justify-content-center">
                                         <Link to="/user/pets/add"
