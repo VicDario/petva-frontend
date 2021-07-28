6ba59ea998b1a906097f915972e286a35983490e
zero-skill <gabbriel.amaya@gmail.com>
2021-07-28 14:48:32 -0400

    foundationpetsinadoption stylized

--- src/views/FoundationPetsInAdoption.js
+++ src/views/FoundationPetsInAdoption.js
@@ -20,8 +20,18 @@ const FoundationPetsInAdoption = () => {
                 <div className="container">
                     <div className="row my-4">
                         <div className="col-12 text-center">
-                            <div>
-                                <h2 className="display-1">Mascotas en Adopción</h2>
+                            <div className="row mb-2">
+                                <div className="col-lg-6 col-lg-6 col-md-12 col-sm-12">
+                                    <h2 className="display-1 text-start mb-0">Mascotas en Adopción</h2>
+                                    <hr classname="hr-foundation-adoption mt-0 mb-4" />
+                                </div>
+                                <div className="col-lg-6 col-md-12 col-sm-12">
+                                    <div className="d-flex justify-content-end me-3">
+                                        <Link to="/foundation/pets/add" className="text-decoration-none badge rounded-pill btn-foundation-pets p-3 m-1 fs-4">
+                                            Agregar Mascota
+                                        </Link>
+                                    </div>
+                                </div>
                             </div>
                             <div className="row justify-content-center">
                                 {
@@ -29,25 +39,25 @@ const FoundationPetsInAdoption = () => {
                                         store.pets.length > 0 ?
                                             store.pets.map((pet, index) => {
                                                 return (
-                                                    <div className="col-sm-6 col-md-4" key={index}>
-                                                        <div className="card mb-3">
+                                                    <div className="col-lg-4 col-md-6 col-sm-6" key={index}>
+                                                        <div className="card card-pet mb-3 pt-3 align-items-center">
                                                             <img
                                                                 src={!!pet.picture ? pet.picture : "/images/default.jpg"}
-                                                                className="card-img-top" alt={pet.name}
-                                                                style={{ height: "50vh" }}
+                                                                className="card-img-top img-pet mt-2" alt={pet.name}
                                                             />
                                                             <div
                                                                 className="card-body">
-                                                                <h5
-                                                                    className="card-title">
-                                                                    {pet.name}
+                                                                <div className="d-flex justify-content-center">
+                                                                    <h2 className="card-title">
+                                                                        {pet.name}
+                                                                    </h2>
                                                                     <span
                                                                         className="card-title fs-3 ">
                                                                         {pet.specie === 'cat' ?
                                                                             <FaCat className="align-top ms-1" /> :
                                                                             <FaDog className="align-top ms-1" />}
                                                                     </span>
-                                                                </h5>
+                                                                </div>
                                                                 <p
                                                                     className="card-text">{!!pet.birth_date ?
                                                                         actions.getEdad(pet.birth_date)
@@ -55,22 +65,25 @@ const FoundationPetsInAdoption = () => {
                                                                 <p
                                                                     className="card-text">{!!pet.code_chip ? pet.code_chip :
                                                                         "No registra codigo de chip"}</p>
-                                                                <p
-                                                                    className="card-text badge rounded-pill bg-success fs-3">
-                                                                    {pet.state === "owned" ? "Con Dueño" : "En adopción"}
-                                                                </p>
-
-                                                                <div
-                                                                    className="d-flex justify-content-around">
+                                                                <div className="d-flex justify-content-around mt-3 mb-2">
                                                                     <Link
                                                                         to={"/foundation/pet/history/" + pet.id}
-                                                                        className="btn btn-primary">
+                                                                        className="btn btn-foundation-pets rounded-pill px-5 py-0 fs-4">
                                                                         Historial
                                                                     </Link>
-                                                                    <Link
-                                                                        to={"/foundation/transfer/" + pet.id} className="btn btn-danger">
-                                                                        Transferir
-                                                                    </Link>
+                                                                </div>
+                                                                <div className="row">
+                                                                    <div
+                                                                        className="col-lg-6 col-md-12 col-sm-12 card-text my-1 badge rounded-pill bg-success fs-5 px-0">
+                                                                        {pet.state === "owned" ? "Con Dueño" : "En adopción"}
+                                                                    </div>
+                                                                    <div
+                                                                        className="col-lg-6 col-md-12 col-sm-12 card-text my-1">
+                                                                        <Link
+                                                                            to={"/foundation/transfer/" + pet.id} className="btn btn-danger btn-transfer badge rounded-pill fs-5">
+                                                                            Transferir
+                                                                        </Link>
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -85,14 +98,10 @@ const FoundationPetsInAdoption = () => {
                                         <LoadingSpiner />
                                 }
                             </div>
-                            <div>
-                                <Link to="/foundation/pets/add" className="text-decoration-none badge rounded-pill bg-success p-3 m-1 fs-4">
-                                    Agregar Mascota
-                                </Link>
-                            </div>
+
                         </div>
                     </div>
-                </div>
+                </div >
             }
             {
                 !store.token &&
