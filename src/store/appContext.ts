78614da640eeb97be14b1afc4c7cadf0bcbbbd56
VicDario <26585356+VicDario@users.noreply.github.com>
2024-12-12 00:49:54 -0300

    feat: export only context

--- src/store/appContext.ts
+++ src/store/appContext.ts
@@ -1,37 +1,3 @@
-import { createContext, useEffect, useState } from "react";
-import getState from "./flux";
+import { createContext } from "react";
 
-export const Context = createContext(null);
-
-const injectContext = (PassedComponent) => {
-    const StoreWrapper = (props) => {
-        const [state, setState] = useState(
-            getState({
-                getStore: () => state.store,
-                getActions: () => state.actions,
-                setStore: (updateStore) =>
-                    setState({
-                        store: Object.assign(state.store, updateStore),
-                        actions: { ...state.actions },
-                    }),
-            })
-        );
-
-        useEffect(() => {
-            
-           /* state.actions.getFoundationDetail(); */
-            state.actions.getPetsInAdoption(1);
-            state.actions.getLostPets(1);
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, [])
-
-        return (
-            <Context.Provider value={state}>
-                <PassedComponent />
-            </Context.Provider>
-        );
-    };
-    return StoreWrapper;
-};
-
-export default injectContext;
+export const Context = createContext({});
