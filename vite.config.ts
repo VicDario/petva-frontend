885ed79b189317d37d6509f068cadaa835b414b1
VicDario <26585356+VicDario@users.noreply.github.com>
2024-12-12 00:47:02 -0300

    chore: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "/src/components"),
+      "@hooks": path.resolve(__dirname, "/src/hooks"),
+      "@pages": path.resolve(__dirname, "/src/pages"),
+    },
+  },
+});
