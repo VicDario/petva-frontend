3c540aedb3af2e5e0a73cf0edb187fecbdf862c0
VicDario <26585356+VicDario@users.noreply.github.com>
2024-12-24 03:02:40 -0300

    fix: update import to correct path

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,10 @@ import "./App.css";
 import Navbar from "@components/navbar/navbar.component";
 import Footer from "@components/footer/footer.component";
 
-import { Context } from "./store/appContext";
+import { Context } from "@context/appContext";
 import useInitialState from "@hooks/useInitialState";
 
 import { AppRoutes } from "./routes/routes";
-
 
 function App() {
   return (
